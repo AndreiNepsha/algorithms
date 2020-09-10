5f84eabce7b12298a99a3329486f172b37726389
Nepsha Andrei <Nepsha.A.Aleksand@sberbank.ru>
2020-09-10 19:28:58 +0300

    sync

--- src/main/kotlin/nepsha/codewars/BlockSequence.kt
+++ src/main/kotlin/nepsha/codewars/BlockSequence.kt
@@ -21,6 +21,44 @@ object BlockSequence {
         return currentNumber.toString()[indexInLastNumber].toString().toInt()
     }
 
+    fun getLastSubSequenceStart(n: Long) {
+        var lastNumber = 9
+        var lastNumberDigits = 1
+        var digitsInSeqWithCurNumberLength = 45
+        var totalDigits = digitsInSeqWithCurNumberLength
+
+
+        while (totalDigits < n) {
+            lastNumber = (lastNumber * 10) + 9
+            lastNumberDigits++
+            digitsInSeqWithCurNumberLength = ((((lastNumber+1) * lastNumber) / 2) * lastNumberDigits) - digitsInSeqWithCurNumberLength
+            totalDigits += digitsInSeqWithCurNumberLength
+        }
+
+        totalDigits -= digitsInSeqWithCurNumberLength
+    }
+
+    fun ttt(startSeqLen: Long, n: Long) {
+        var curSeqLen = 1L
+        var curSeqDigits = 1L
+        var curSeqMaxNumberDigits = 1L
+        var curSeqMaxNumberCeil = 10L
+        var totalDigits = 1L
+
+        while (totalDigits < n) {
+            curSeqLen++
+            if (curSeqLen >= curSeqMaxNumberCeil) {
+                curSeqMaxNumberCeil *= 10
+                curSeqMaxNumberDigits++
+            }
+            curSeqDigits += curSeqMaxNumberDigits
+
+            totalDigits += curSeqDigits
+        }
+
+        val indInLastSeq = n - (totalDigits - curSeqDigits)
+    }
+
     fun solve(n: Long): Int {
         var curSeqLen = 1L
         var curSeqDigits = 1L
---
+++ src/main/kotlin/nepsha/codewars/MaximumSubarraySum.kt
@@ -0,0 +1,8 @@
+package nepsha.codewars
+
+object MaximumSubarraySum {
+    fun maxSequence(arr: List<Int>): Int {
+        return 0
+    }
+}
+
