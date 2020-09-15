99c4c79433076dad143d58bbedf25bce9a178da6
Nepsha Andrei <Nepsha.A.Aleksand@sberbank.ru>
2020-09-15 17:55:29 +0300

    + Maximum Subarray Sum done

--- src/main/kotlin/nepsha/codewars/MaximumSubarraySum.kt
+++ src/main/kotlin/nepsha/codewars/MaximumSubarraySum.kt
@@ -2,7 +2,17 @@ package nepsha.codewars
 
 object MaximumSubarraySum {
     fun maxSequence(arr: List<Int>): Int {
-        return 0
+        var maxSum = 0
+        var curSum = 0
+
+        arr.forEach {
+            curSum += it
+
+            if (curSum < 0) curSum = 0
+            if (curSum > maxSum) maxSum = curSum
+        }
+
+        return maxSum
     }
 }
 
